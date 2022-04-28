6e0b76d4bb395966b21ce4632cae71fdbeaac454
Maryna Khudorozhkova <kenimaaa@gmail.com>
2022-04-28 20:48:43 +0300

    Finish sixth task

---
+++ js/task-06.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('#validation-input');
+
+const valid = () => {
+
+    input.classList.add('valid')
+    if (input.classList.contains('invalid')) {
+        input.classList.remove('invalid')
+    }  
+}
+
+const invalid = () => {
+
+    input.classList.add('invalid')
+    if (input.classList.contains('valid')) {
+        input.classList.remove('valid')
+    }
+}
+input.addEventListener('blur', () => {
+    
+    if (input.value.length >= input.dataset.length) {
+        valid();
+    } else { invalid(); }
+    
+})
+
+
