6c883fdf4fb1270e9519416e68ef62efa7bb45d3
Maryna Khudorozhkova <kenimaaa@gmail.com>
2022-04-29 01:03:42 +0300

    Finish ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const btnColor = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+function handleClickButtonColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+}
+btnColor.addEventListener('click', handleClickButtonColor);
