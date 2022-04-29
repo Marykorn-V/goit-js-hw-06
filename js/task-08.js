7fa1dbcc6b43d5695007db886215b3a4a81200f8
Maryna Khudorozhkova <kenimaaa@gmail.com>
2022-04-30 00:31:25 +0300

    Corrected eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -10,7 +10,12 @@ function handleSubmit(event) {
   if (email.value === "" || password.value === "") {
     return alert("Please fill in all the fields!");
   }
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
+  const dataObj = {
+    email: email.value,
+    password: password.value,
+  }
+  console.log(dataObj);
+  
     event.currentTarget.reset();
 }
 
